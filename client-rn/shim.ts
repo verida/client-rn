3c9455d7e0c042f6ea3650efc8cd0f90285085cd
Chris <chris@verida.io>
2021-09-07 08:14:36 +0930

    Use base-64 instead of did-jwt for base64 encode/decode.

--- client-rn/shim.ts
+++ client-rn/shim.ts
@@ -25,26 +25,6 @@ if (typeof localStorage !== 'undefined') {
 if (typeof TextEncoder === 'undefined') global.TextEncoder = require('text-encoding-shim').TextEncoder
 if (typeof TextDecoder === 'undefined') global.TextDecoder = require('text-encoding-shim').TextDecoder
 
-// If using the crypto shim, uncomment the following line to ensure
-// crypto is loaded first, so it can populate global.crypto
-// require('crypto')
-
-import { base64ToBytes, bytesToBase64 } from "did-jwt/lib/util"
-
-if (!global.btoa) {
-    global.btoa = bytesToBase64;
-}
-
-if (!global.atob) {
-    global.atob = base64ToBytes;
-}
-
-/*
-Note: @verida/datastore had the following code, but
-I have replaced to use the did-jwt encoding libaries above
-
-(Chris - 25 Aug '21)
-
 import {decode, encode} from 'base-64'
 
 if (!global.btoa) {
@@ -54,4 +34,3 @@ if (!global.btoa) {
 if (!global.atob) {
     global.atob = decode;
 }
-*/
